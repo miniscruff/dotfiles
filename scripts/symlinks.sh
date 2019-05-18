1142835c75ee8ea769c004df68077c7e6eb39107
Ronnie Smith <halfpint1170@gmail.com>
2019-05-18 11:48:58 -0700

    Add neofetch config file

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,10 +1,12 @@
 cd settings
 
+mkdir $HOME/.config/neofetch
 mkdir $HOME/.config/nvim
 mkdir $HOME/.config/termite
 
 ln .zshrc $HOME/.zshrc
 ln .config/nvim/init.vim $HOME/.config/nvim/init.vim
+ln .config/neofetch/config.conf $HOME/.config/neofetch/config.conf
 ln .config/termite/config $HOME/.config/termite/config
 
 cd ..
