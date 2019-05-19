02f332b0cf8d72023980ed32f8cf4cbfe69bc768
Ronnie Smith <halfpint1170@gmail.com>
2019-05-18 22:34:40 -0700

    Set scripts to be executable, add ranger and conf

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -4,6 +4,7 @@ mkdir $HOME/.config/i3
 mkdir $HOME/.config/i3status
 mkdir $HOME/.config/neofetch
 mkdir $HOME/.config/nvim
+mkdir $HOME/.config/ranger
 mkdir $HOME/.config/termite
 
 ln .zshrc $HOME/.zshrc
@@ -12,6 +13,7 @@ ln .config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln .config/neofetch/config.conf $HOME/.config/neofetch/config.conf
 ln .config/i3/config $HOME/.config/i3/config
 ln .config/termite/config $HOME/.config/termite/config
+ln .config/ranger/rc.conf $HOME/.config/ranger/rc.conf
 ln .config/i3status/config $HOME/.config/i3status/config
 
 cd ..
