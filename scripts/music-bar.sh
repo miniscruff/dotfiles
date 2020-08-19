e42a6837e364dcaa446daa6080648158a363a231
Ronnie Smith <halfpint1170@gmail.com>
2020-08-19 11:49:49 -0700

    Fix issue with i3 and paused youtube music

--- scripts/music-bar.sh
+++ scripts/music-bar.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-window_name=$(xdotool search --name "YouTube Music" | xargs -l xprop -id | grep _NET_WM_NAME)
-if [ -z "${window_name}" ]; then
+wm_line=$(xdotool search --name "YouTube Music" | xargs -l xprop -id | grep _NET_WM_NAME)
+if [ -z "${wm_line}" ]; then
     exit 0
 fi
-IFS='-' read -ra splits <<< "${window_name}"
-if [ -z "${splits[0]}" ]; then
-    exit 0
+
+title=$(echo ${wm_line} | cut -c 30- | tr -d '"')
+if [ "$title" = "YouTube Music" ]; then
+    echo ♪ paused ♪
+else
+    song_name=$(echo $title | sed 's/ - YouTube Music//')
+    echo ♪ ${song_name} ♪
 fi
-echo ♪ $(echo ${splits[0]} | cut -c 30-) ♪
