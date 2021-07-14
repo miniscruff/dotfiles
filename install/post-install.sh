536f8b8ed7a8766b01232d3f55df6e689cb1ded0
Ronnie Smith <halfpint1170@gmail.com>
2021-07-13 20:13:11 -0700

    update qmk to fix setup args

--- install/post-install.sh
+++ install/post-install.sh
@@ -2,5 +2,5 @@ nvim --headless +PlugInstall +qa
 pip install jedi neovim
 pipx install httpie poetry
 pipx install qmk
-qmk setup miniscruff/qmk_firmware ./packages/qmk_firmware --branch miniscruff -y
+qmk setup miniscruff/qmk_firmware --branch miniscruff -y
 git config --global core.excludesfile ~/.gitignore
