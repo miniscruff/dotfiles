2b795062ba46747ab52d9205112911e87e86ddea
Ronnie Smith <miniscruff@hey.com>
2022-07-24 18:49:36 -0700

    add i3 lock and youtube music

--- install/post-install.sh
+++ install/post-install.sh
@@ -5,3 +5,4 @@ set -exu
 git config --global core.excludesfile ~/.gitignore
 nvim --headless +PlugInstall +UpdateRemotePlugins +qa
 pip3 install jedi neovim
+sudo snap install youtube-music-desktop-app
