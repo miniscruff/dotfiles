e51683de89d4c8e20afa71cb9b79850e14f0ec8c
Ronnie Smith <miniscruff@hey.com>
2022-12-20 21:41:37 -0800

    fix git clone path

--- install/git-packages.sh
+++ install/git-packages.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 set -exu
 
-if [ ! -d "~/catpucchin/alacritty" ] ; then
+if [ ! -d "~/projects/catpucchin/alacritty" ] ; then
     gh repo clone catpucchin/alacritty -- ~/projects/catpucchin/alacritty
 fi
 
-if [ ! -d "~/vivien/i3blocks-contrib" ] ; then
+if [ ! -d "~/projects/vivien/i3blocks-contrib" ] ; then
     gh repo clone vivien/i3blocks-contrib -- ~/projects/vivien/i3blocks-contrib
 fi
 
-if [ ! -d "~/miniscruff/changie" ] ; then
+if [ ! -d "~/projects/miniscruff/changie" ] ; then
     gh repo clone miniscruff/changie -- ~/projects/miniscruff/changie
 fi
 
-if [ ! -d "~/miniscruff/changie-action" ] ; then
+if [ ! -d "~/projects/miniscruff/changie-action" ] ; then
     gh repo clone miniscruff/changie-action -- ~/projects/miniscruff/changie-action
 fi
 
-if [ ! -d "~/miniscruff/genenv" ] ; then
+if [ ! -d "~/projects/miniscruff/genenv" ] ; then
     gh repo clone miniscruff/genenv -- ~/projects/miniscruff/genenv
 fi
